1b6009cdb83f1f1af90f439ca3bcabcaa8dffa61
Ritchie Vincent <rfcvincent@gmail.com>
2014-12-02 12:25:57 +0000

    Add timer

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,28 @@
 $(function () { //Wait for the document to be ready
 
+    //---------------------------Variables-----------------------------
     $.fx.speeds.slow = 800; //Sets speeds of transitions
     $.fx.speeds.xslow = 1200;
+    var clicked = "1";
+    var questionCount = 0; //This variable stores how many questions the system has asked
+    var questionsLength = Object.keys(text.questions).length; //This variable stores the length of the questions array
+    var randomChoice; //This variable stores the random number
+    var questionParty; //This variable stores the chosen question's corresponding party
+    var labourCount = 0;
+    var conservativeCount = 0;
+    var libdemCount = 0;
+    var ukipCount = 0;
+    var correctCount = 0;
+    var finalPercent = 0;
+    var userScore = 0; //Sets the users score to 0, since it's the start of the quiz!
+    var removedClasses = "labourChoice conservativeChoice libdemChoice ukipChoice"; //Sets the classes to remove (All other parties)
+    var audioCorrect = document.getElementById("correctAudio"); //Sets the variable to the appropriate audio file
+    var audioIncorrect = document.getElementById("incorrectAudio");
+    var audioTimeUp = document.getElementById("timeUpAudio");
+    var audioResult = document.getElementById("resultAudio");
+    //---------------------------------------------------------------------------
+
+
 
     $(".headerDownBtn").click(function () { //Scrolls the page down when clicking the down arrow in the header
         $('html,body').animate({
@@ -19,8 +40,6 @@ $(function () { //Wait for the document to be ready
         $("#questionSection").slideToggle("slow");
         $("#questionSection").addClass("" + choiceClassQuestionSection + "");
     };
-
-    var clicked = "1";
 
     $(".partyBtn").click(function (e) {
         var idClicked = e.target.id;
@@ -64,31 +83,39 @@ $(function () { //Wait for the document to be ready
             },
             'xslow');
 
-        var questionCount = 0; //This variable stores how many questions the system has asked
 
-        var questionsLength = Object.keys(text.questions).length; //This variable stores the length of the questions array
+        function newQuestion() {
+            if (questionCount < 10) { //Only displays a new question if the question count is below 10
+                var wait = setTimeout(function () {
+                    $("#questionSection").removeClass(removedClasses);
+                    randomQuestion(); //Runs the randomQuestion function, passing the randomised number to it, to create a new random number
+                    timer();
+                    $(".correctParty").html(""); //Removes the content of the answer message
+                    $('.correctParty').fadeTo(200, 0); //Hides the white "Correct" or "Actually this is..." bar
+                }, 4000); //Waits 4 seconds before running this function
 
-        var randomChoice; //This variable stores the random number
+                wait(); //Runs the wait function above
 
-        var questionParty; //This variable stores the chosen question's corresponding party
+            } else {
+                $('.choiceBtn').prop('disabled', true); //Disables the agree/disagree selection buttons so no user can re-click to answer another question
+                $("#questionSection").slideToggle(3000, function () {
+                    resultDisplay();
+                });
+            }
+        }
 
 
+        function timer() {
+            $(".loadingBar").addClass("loadingBarAnimate");
+            $(".loadingBar").one($.support.transition.end,
+                function () {
+                    $(".loadingBar").removeClass("loadingBarAnimate");
+                    wrongChoiceColour("timeUp", "");
+                    newQuestion();
+                });
+        }
 
-        //    $(".loadingBar").addClass("loadingBarAnimate");
-        //    $(".loadingBar").one($.support.transition.end,
-        //        function () {
-        //            $(".loadingBar").removeClass("loadingBarAnimate");
-        //            alert("Time's up!");
-        //            $(".loadingBar").addClass("loadingBarAnimate");
-        //        });
-
-        var labourCount = 0;
-        var conservativeCount = 0;
-        var libdemCount = 0;
-        var ukipCount = 0;
-        var correctCount = 0;
-        var finalPercent = 0;
-
+        timer(); //Runs the timer function to start the timer
         randomQuestion(); //Runs the random question function to display the first question
 
         function randomQuestion() { //Runs a function that randomises the JSON objects to display a random question
@@ -111,7 +138,7 @@ $(function () { //Wait for the document to be ready
             $('.choiceBtn').prop('disabled', false); //Re-enables the choice button once the new question is displayed
         }
 
-        var userScore = 0; //Sets the users score to 0, since it's the start of the quiz!
+
 
         function zeroCheck(partyName) { //This function checks if the finalPercent value is not a NaN% (Not a Number) error
             if (isNaN(finalPercent)) { //Checks if it's not a number (Which it will be if there have been no questions of the chosen party asked)
@@ -120,6 +147,7 @@ $(function () { //Wait for the document to be ready
         }
 
         function resultDisplay() {
+            audioResult.play();
             $("#resultSection").slideDown("xslow", function () {
                 window.scrollTo(0, document.body.scrollHeight); //This scrolls the view to the bottom of the page, to show the results to the user easier
             });
@@ -127,88 +155,83 @@ $(function () { //Wait for the document to be ready
                 finalPercent = (correctCount / labourCount) * 100; //Takes the amount correctly answered and the amount asked and calculates the percent correctly answered
                 finalPercent = finalPercent.toFixed(); //This converts the number to 0 decimal places
                 zeroCheck(labourCount); //Runs the zeroCheck function
-                $(".scoreNumber").html("<p>You scored " + finalPercent + "% of the Labour questions correct.</p>"); //Displays the percent correct to the user
+                $(".scoreNumber").html("<p>You got " + finalPercent + "% of the Labour questions correct.</p>"); //Displays the percent correct to the user
                 $(".scoreNumber").append("<p>Your overall score is: " + userScore + "</p>");
             }
             if (choiceClass == "conservativeChoice") {
                 finalPercent = (correctCount / conservativeCount) * 100;
                 finalPercent = finalPercent.toFixed();
                 zeroCheck(conservativeCount);
-                $(".scoreNumber").html("<p>You scored " + finalPercent + "% of the Conservative questions correct.</p>");
+                $(".scoreNumber").html("<p>You got " + finalPercent + "% of the Conservative questions correct.</p>");
                 $(".scoreNumber").append("<p>Your overall score is: " + userScore + "</p>");
             }
             if (choiceClass == "libdemChoice") {
                 finalPercent = (correctCount / libdemCount) * 100;
                 finalPercent = finalPercent.toFixed();
                 zeroCheck(libdemCount);
-                $(".scoreNumber").html("<p>You scored " + finalPercent + "% of the Liberal Democrat questions correct.</p>");
+                $(".scoreNumber").html("<p>You got " + finalPercent + "% of the Liberal Democrat questions correct.</p>");
                 $(".scoreNumber").append("<p>Your overall score is: " + userScore + "</p>");
             }
             if (choiceClass == "ukipChoice") {
                 finalPercent = (correctCount / ukipCount) * 100;
                 finalPercent = finalPercent.toFixed();
                 zeroCheck(ukipCount);
-                $(".scoreNumber").html("<p>You scored " + finalPercent + "% of the UKIP questions correct.</p>");
+                $(".scoreNumber").html("<p>You got " + finalPercent + "% of the UKIP questions correct.</p>");
                 $(".scoreNumber").append("<p>Your overall score is: " + userScore + "</p>");
             }
         }
+
+
 
         $(".choiceBtn").click(function (e) { //Runs this function when the agreeChoice or disagreeChoice button is clicked
             $('.choiceBtn').prop('disabled', true); //Disables the choice button (although temporarily) until the next question is displayed
             var id = this.id; //Sets a variable called id to the id of the clicked button (agreeChoice or disagreeChoice)
             checkQuestion(id, questionParty, choiceClass); //Calls the checkQuestion function and passes the variables as parameters
-            if (questionCount < 1) { //Only displays a new question if the question count is below 10
-                var wait = setTimeout(function () {
-                    $("#questionSection").removeClass(removedClasses);
-                    randomQuestion(); //Runs the randomQuestion function, passing the randomised number to it, to create a new random number
-                    $(".correctParty").html(""); //Removes the content of the answer message
-                    $('.correctParty').fadeTo(200, 0); //Hides the white answer bar
-                }, 400);
-
-                wait(); //Runs the wait function above
-
-            } else {
-                $('.choiceBtn').prop('disabled', true); //Disables the agree/disagree selection buttons so no user can re-click to answer another question
-                wrongChoiceColour(questionParty, choiceClass);
-                $("#questionSection").slideToggle(3000, function () {
-                    resultDisplay();
-                });
-            }
+            newQuestion();
         });
 
-        var removedClasses = "labourChoice conservativeChoice libdemChoice ukipChoice"; //Sets the classes to remove (All other parties)
+
 
         function wrongChoiceColour(questionParty, choiceClass) { //This function checks which party you have chosen and will remove any previous classes relating to other parties. It then replaces it with the new party class, which styles the background of the section, alerting the user to the party that question actually relates to
-
             if (questionParty == "labourChoice") {
                 $("#questionSection").removeClass(removedClasses); //Removes old classes using the "removedClasses" variable defined above
                 $("#questionSection").addClass("labourChoice"); //Adds the party class which relates to the question they have just answered
                 $('.correctParty').fadeTo(500, 1); //Fades the answer to the question to "1" opacity over 500 miliseconds
                 $(".correctParty").html("<p>Actually, this is a Labour policy!</p>"); //Adds some text to tell the user the answer to the question
+                audioIncorrect.play(); //Plays the "incorrectAudio" file
             }
             if (questionParty == "conservativeChoice") {
                 $("#questionSection").removeClass(removedClasses);
                 $("#questionSection").addClass("conservativeChoice");
                 $('.correctParty').fadeTo(500, 1);
                 $(".correctParty").html("<p>Actually, this is a Conservative policy!</p>");
+                audioIncorrect.play();
             }
             if (questionParty == "libdemChoice") {
                 $("#questionSection").removeClass(removedClasses);
                 $("#questionSection").addClass("libdemChoice");
                 $('.correctParty').fadeTo(500, 1);
                 $(".correctParty").html("<p>Actually, this is a Liberal Democrat policy!</p>");
+                audioIncorrect.play();
             }
             if (questionParty == "ukipChoice") {
                 $("#questionSection").removeClass(removedClasses);
                 $("#questionSection").addClass("ukipChoice");
                 $('.correctParty').fadeTo(500, 1);
                 $(".correctParty").html("<p>Actually, this is a UKIP policy!</p>");
+                audioIncorrect.play();
+            }
+            if (questionParty == "timeUp") {
+                $('.correctParty').fadeTo(500, 1);
+                $(".correctParty").html("<p>You ran out of time!</p>");
+                audioTimeUp.play(); //Plays the "timeUp" audio file
             }
         }
 
         function correctAnswer() { //Shows a "Correct!" message when the user gets the answer correct
             $('.correctParty').fadeTo(500, 1);
             $(".correctParty").html("<p>Correct!</p>");
+            audioCorrect.play();
         }
 
 
@@ -221,19 +244,24 @@ $(function () { //Wait for the document to be ready
             //            alertParty = alertParty.capitalise(); //Runs the capitalise function
             if (id == "agreeChoice") { //If the user has clicked the "Agree" button
                 if (questionParty == choiceClass) { //If the question's party matches the user's chosen party
+                    $(".loadingBar").removeClass("loadingBarAnimate");
                     userScore++; //Raises the user's score since they got the question correct
                     correctCount++;
                     correctAnswer(); //Runs the correctAnswer function to display the "Correct!" message
                 } else {
+                    $(".loadingBar").removeClass("loadingBarAnimate");
                     wrongChoiceColour(questionParty, choiceClass); //Calls the function above that checks the party against the question and changes the background style accordingly
                     userScore--; //Lowers the user's score since they got the question incorrect
                 }
             }
             if (id == "disagreeChoice") { //If the user has clicked the "Disagree" button
                 if (questionParty == choiceClass) { //If the question's party matches the user's chosen party
+                    $(".loadingBar").removeClass("loadingBarAnimate");
                     wrongChoiceColour(questionParty, choiceClass);
                     userScore--;
                 } else {
+                    $(".loadingBar").removeClass("loadingBarAnimate");
+                    userScore++;
                     correctAnswer();
                 }
             }
